d8f0273a6810f21d918fecb0198085eb980aee15
Abdel <juniorperezpy@gmail.com>
2020-10-22 18:43:04 -0300

    Move App.test to test folder

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import store from './app/store';
-import App from './App';
+import store from '../redux/store';
+import App from '../components/App';
 
 test('renders learn react link', () => {
   const { getByText } = render(
