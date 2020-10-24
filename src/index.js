dd28dd7fc3bc9973c606c5a5b7a0b33614816401
Abdel <juniorperezpy@gmail.com>
2020-10-24 18:29:07 -0300

    Update bootstrap import before index.css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import store from './redux/store';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
