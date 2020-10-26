f7f6e95c6293309638c63b8c8956436b57574586
Abdel <juniorperezpy@gmail.com>
2020-10-26 11:43:15 -0300

    Add structure for HomePage content
    - Header with expandible search bar
    - text content
    - controls to book a massage session (dropdown and button)

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -13,7 +13,26 @@ const HomePage = () => (
       </form>
     </header>
     <div className='home-content'>
-      <h1>Schedule your appointment with the best professionals</h1>
+      <div className='content'>
+        <h2>BOOK A MASSAGE SESSION</h2>
+        <hr />
+        <p>
+          We utilize the latest techniques to provide cost-effective and time conscious care. A variety of treatment modalities are utilized as needed for symptom reduction. We are participating providers for Blue Cross and Blue Shield of North Dakota, ND PERS, Medicare, and a variety of other managed care plans.
+        </p>
+        <div className='controls'>
+          <div className="select"> 
+            <select name="slct" id="slct">
+              <option value="1">London</option>
+              <option value="2">Paris</option>
+              <option value="3">New York</option>
+              <option value="4">Rio</option>
+            </select>
+          </div>
+          <button>
+            Book Now
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 );
