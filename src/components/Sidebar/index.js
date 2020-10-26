ea8a51c31bce4d022d02040725fcabcedfb42e14
Abdel <juniorperezpy@gmail.com>
2020-10-26 11:46:43 -0300

    Add sidebar component complete

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
+
+import twitterLogo from '../../assets/icons/twitter.svg';
+import facebookLogo from '../../assets/icons/facebook.svg';
+import googleplusLogo from '../../assets/icons/google-plus.svg';
+import vimeoLogo from '../../assets/icons/vimeo.svg';
+import pinterestLogo from '../../assets/icons/pinterest.svg';
+
+const Sidebar = () => (
+  <nav className="sidebar">
+    <label htmlFor="hamburger" className="hamburger-icon">&#9776;</label>
+    <input type="checkbox" id="hamburger" name="hamburger" />
+  
+    <div className="nav-items">
+      <div className="links-container">
+        <NavLink exact to={ROUTES.HOME} activeClassName='active'>HOME</NavLink>
+        <NavLink to={ROUTES.THERAPISTS} activeClassName='active'>THERAPISTS</NavLink>
+        <NavLink to={ROUTES.SPECIALTIES} activeClassName='active'>SPECIALTIES</NavLink>
+        <NavLink to={ROUTES.LOGIN} activeClassName='active'>LOGIN</NavLink>
+        <NavLink to={ROUTES.SIGNUP} activeClassName='active'>SIGNUP</NavLink>
+        <NavLink to={ROUTES.ABOUT} activeClassName='active'>ABOUT</NavLink>
+      </div>
+    </div>
+
+    <footer className="sm-links">
+      <div>
+        <ul className="nav">
+          <li>
+            <a href="www.twitter.com">
+              <img src={twitterLogo} alt='twitter-logo' />
+            </a>
+          </li>
+          <li>
+            <a href="www.twitter.com">
+              <img src={facebookLogo} alt='facebook-logo' />
+            </a>
+          </li>
+          <li>
+            <a href="www.twitter.com">
+              <img src={googleplusLogo} alt='googleplus-logo' />
+            </a>
+          </li>
+          <li>
+            <a href="www.twitter.com">
+              <img src={vimeoLogo} alt='vimeo-logo' />
+            </a>
+          </li>
+          <li>
+            <a href="www.twitter.com">
+              <img src={pinterestLogo} alt='pinterest-logo' />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className='copyright'>
+        <p>&copy; 2015 PIAGGIO & C.S.P.A - P.IVA</p>
+      </div>
+    </footer>
+  </nav>
+);
+
+export default Sidebar;
