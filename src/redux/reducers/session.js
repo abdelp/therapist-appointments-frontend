c4ad0d7cd392315db444095ad2ee7fba806d1980
Abdel <juniorperezpy@gmail.com>
2020-10-28 18:39:56 -0300

    Add reducer for logout action

--- src/redux/reducers/session.js
+++ src/redux/reducers/session.js
@@ -1,19 +1,23 @@
-import { LOGIN_USER } from '../actionTypes';
+import { LOGIN_USER, LOGOUT_USER } from '../actionTypes';
 
 const initialState = {
   username: null,
   email: null,
-  token: null
+  token: null,
 };
 
 export default function (state = initialState, action) {
-  console.log(action);
-
   switch (action.type) {
     case LOGIN_USER: {
       return {
         ...state,
-        user: action.payload.content,
+        ...action.payload.content,
+      };
+    }
+    case LOGOUT_USER: {
+      return {
+        ...state,
+        ...initialState,
       };
     }
     default:
