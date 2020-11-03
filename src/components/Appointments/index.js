cc70dab141c7f8086a81069b5b14858398f78817
Abdel <juniorperezpy@gmail.com>
2020-11-03 19:59:06 -0300

    Add Appointments Component

---
+++ src/components/Appointments/index.js
@@ -0,0 +1,73 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getAppointments } from '../../APIs';
+
+import { AddAppointments } from '../../redux/actions';
+
+const Appointments = ({
+  appointments, userId, token, AddAppointments,
+}) => {
+  const handleFetch = async () => {
+    try {
+      const result = await getAppointments(userId, token);
+      AddAppointments(result);
+    } catch (e) {
+      throw new Error(e);
+    }
+  };
+
+  useEffect(() => {
+    handleFetch();
+  }, []);
+
+  return (
+    <div id="appointments-page">
+      <h1 className="text-center">Appointments</h1>
+      <div className="appointments-container">
+        <div className="titles">
+          <div>
+            <h3>
+              Therapist
+            </h3>
+          </div>
+          <div>
+            <h3>
+              Date
+            </h3>
+          </div>
+        </div>
+        <div className="appointments-list">
+          {appointments.map(a => (
+            <div className="appointment-item" key={a.id}>
+              <div>{a.therapist_name}</div>
+              <div>{a.start_at}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  const {
+    appointments: {
+      appointments,
+      isLoading,
+      isError,
+    },
+    session: {
+      token,
+      id: userId,
+    },
+  } = state;
+
+  return {
+    appointments, isLoading, isError, token, userId,
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { AddAppointments },
+)(Appointments);
