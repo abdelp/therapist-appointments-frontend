9ba403bc25f7c976d876807115a8b905780e97e7
Abdel <juniorperezpy@gmail.com>
2020-11-04 17:37:30 -0300

    Fix tests for API methods

--- src/test/redux/actions.test.js
+++ src/test/redux/actions.test.js
@@ -1,14 +1,15 @@
 import * as actions from '../../redux/actions';
 import * as types from '../../redux/actionTypes';
 import userData from './user-data.json';
+import therapists from './therapists.json';
 
 describe('actions', () => {
   it('should create an action to login a user', () => {
     const expectedAction = {
       type: types.LOGIN_USER,
       payload: {
-        userData,
-      }
+        content: userData,
+      },
     };
 
     expect(actions.loginUser(userData)).toEqual(expectedAction);
@@ -25,18 +26,17 @@ describe('actions', () => {
   it('should create an action to add therapists to the store', () => {
     const expectedAction = {
       type: types.FETCH_SUCCESS,
+      payload: { therapists },
     };
 
-    expect(actions.AddTherapists(types.FETCH_SUCCESS)).toEqual(expectedAction);
+    expect(actions.AddTherapists(therapists)).toEqual(expectedAction);
   });
 
   it('should create an actions to indicate that the fetching initialized', () => {
     const expectedAction = {
-      type: types.FETCH_INIT
+      type: types.FETCH_INIT,
     };
 
     expect(actions.FetchInit(types.FETCH_INIT)).toEqual(expectedAction);
   });
-
-
 });
