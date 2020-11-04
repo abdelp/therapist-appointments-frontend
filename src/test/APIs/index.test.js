56849c4174b1fd710a8d60971b93537ca9704617
Abdel <juniorperezpy@gmail.com>
2020-11-04 12:47:19 -0300

    Add getTherapists spec test

--- src/test/APIs/index.test.js
+++ src/test/APIs/index.test.js
@@ -3,8 +3,7 @@ import {
   createAppointment, getAppointments, getTherapists, signinUser,
 } from '../../APIs';
 import signedInData from './signed-in.json';
-// import pokemon from './pokemon.json';
-// import result from './result.json';
+import therapists from './therapists.json';
 
 jest.mock('axios');
 
@@ -15,11 +14,23 @@ describe('index API', () => {
     await expect(signinUser({ username: 'abdel', password: '123456' })).resolves.toEqual(signedInData);
   });
 
-  // it('fetches erroneously data from the API', async () => {
-  //   const errorMessage = 'Network Error';
+  it('fetches erroneously data from the API', async () => {
+    const errorMessage = 'Network Error';
+    // eslint-disable-next-line prefer-promise-reject-errors
+    axios.post.mockImplementationOnce(() => Promise.reject({
+      response: {
+        data: {
+          error: errorMessage,
+        },
+      },
+    }));
 
-  //   axios.get.mockImplementationOnce(() => Promise.reject(new Error(errorMessage)));
+    await expect(signinUser({ username: 'abdel', password: '123456' })).rejects.toThrow(errorMessage);
+  });
 
-  //   await expect(getPokemons()).rejects.toThrow(errorMessage);
-  // });
+  it('gets appointments of user', async () => {
+    axios.get.mockImplementationOnce(() => Promise.resolve(therapists));
+
+    await expect(getTherapists()).resolves.toEqual(therapists.data);
+  });
 });
